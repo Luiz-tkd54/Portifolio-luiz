ca65853725cc845a4d7b40e0b1239d6b368e3cd9
Luiz Henrique <lhrodrigues956@gmail.com>
2025-01-10 09:54:40 -0300

    adicionado funcao ao botao

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-alert
+function togglemode(){
+    const html = document.documentElement
+
+    // classlist.contains = caso tenha retorna um valor de verdadeiro ou falso
+    if(html.classList.contains('light')){
+        // se tiver
+        html.classList.remove('light')
+    }else{
+        html.classList.add('light')
+    }
+}
